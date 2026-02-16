eb4b773873cbba16edb5d2336d59dcaa9bdf7546
Vladislav <vlad-ski-96borsh@ya.ru>
2026-02-16 21:15:15 +0900

    port

--- api/src/server.ts
+++ api/src/server.ts
@@ -20,6 +20,10 @@ app.use(express.json({ limit: '2mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+app.get('/health', (_req, res) => {
+  res.json({ ok: true })
+})
+
 app.use('/auth', authRouter)
 app.use('/news', newsRouter)
 app.use('/schedule', scheduleRouter)
@@ -30,7 +34,7 @@ app.use((err: unknown, _req: express.Request, res: express.Response, _next: expr
   res.status(500).json({ error: message })
 })
 
-app.listen(env.port, () => {
+app.listen(env.port, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log(`API listening on ${env.publicBaseUrl}`)
 })
